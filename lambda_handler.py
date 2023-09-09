49966e47091bdeb754b527e3d0c983d1e8af6285
tomasesber <tomas.esber@duke.edu>
2023-09-09 18:39:44 -0400

    Refactor method to getInformationForRooms and delete irrelevant endpoint.

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,12 +1,9 @@
 from lib import sender, database_access
 
-# defines the methods mapped to lambda and accessed by API gateway endpoint.
+# defines the methods mapped to lambda and accessed through API gateway endpoint.
 
 def handle_send_request(event, context):
     return sender.send_message("")
-
-def handle_getEmployeesByIds_request(event, context):
-    return database_access.getEmployeesByIds("")
 
 def handle_getEmployeesInRoom_request(event, context):
     return database_access.getEmployeesInRoom("", "")
@@ -14,8 +11,8 @@ def handle_getEmployeesInRoom_request(event, context):
 def handle_getHospitals_request(event, context):
     return database_access.getHospitals()
 
-def handle_getNumberOfEmployeesInRooms_request(event, context):
-    return database_access.getNumberOfEmployeesInRooms("")
+def handle_getInformationForRooms_request(event, context):
+    return database_access.getInformationForRooms("")
 
 def handle_getRoomsInHospital_request(event, context):
-    return database_access.getRoomsInHospital("")
+    return database_access.getRoomsInHospital(event)
--- lib/database_access.py
+++ lib/database_access.py
@@ -1,15 +1,12 @@
 # defines database access layer
 
-def getEmployeesByIds(employee_id) -> {}:
-    return {}
-
 def getEmployeesInRoom(hospital_id, room_id) -> {}:
     return {}
 
 def getHospitals() -> {}:
     return {}
 
-def getNumberOfEmployeesInRooms(hospital_id) -> {}:
+def getInformationForRooms(hospital_id) -> {}:
     return {}
 
 def getRoomsInHospital(hospital_id) -> {}:
